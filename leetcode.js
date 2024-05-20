a436a15b8cb295432d98104e3473e2380bb62d18
alxs1dd93 <alxs1dd93@gmail.com>
2024-05-20 21:50:24 +0700

    Remove Element, removeDuplicates

--- leetcode.js
+++ leetcode.js
@@ -350,3 +350,73 @@ const merge = (nums1, m, nums2, n) => {
   nums2.slice(0, n).forEach(num => nums1.push(num));
   return nums1.sort((a, b) => a - b); 
 };
+
+
+// Remove Element
+// https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
+
+
+// Example 1:
+
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+// Note that the five elements can be returned in any order.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+const removeElement = (nums, val) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i]; 
+      count++;
+    }
+  }
+  return count;
+};
+
+// Remove Duplicates from Sorted Array
+// https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+// Example 2:
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = (nums) => {
+    if (nums.length === 0) return 0;
+    
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i]!== nums[i - 1]) {
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
+};
+
